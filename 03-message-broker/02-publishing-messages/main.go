2f6af7a356b3d5c7851b045656f2e896df09c0e8
coobeet <coobeet@gmail.com>
2023-07-05 00:33:29 +0800

    feat: 03-03

---
+++ 03-message-broker/02-publishing-messages/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+const topic = "progress"
+
+func main() {
+	logger := watermill.NewStdLogger(false, false)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	publisher, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	msg := message.NewMessage(watermill.NewULID(), []byte("50"))
+	err = publisher.Publish(topic, msg)
+	if err != nil {
+		panic(err)
+	}
+
+	msg = message.NewMessage(watermill.NewULID(), []byte("100"))
+	err = publisher.Publish(topic, msg)
+	if err != nil {
+		panic(err)
+	}
+}
