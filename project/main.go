e7954127f7ef9c07128ceb9a164a38fb275afa54
coobeet <coobeet@gmail.com>
2023-07-05 01:22:47 +0800

    feat: 04-05

--- project/main.go
+++ project/main.go
@@ -94,6 +94,10 @@ func main() {
 
 	e := commonHTTP.NewEcho()
 
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
+
 	e.POST("/tickets-confirmation", func(c echo.Context) error {
 		var request TicketsConfirmationRequest
 		err := c.Bind(&request)
@@ -127,6 +131,8 @@ func main() {
 	})
 
 	g.Go(func() error {
+		<-router.Running()
+
 		err := e.Start(":8080")
 		if err != nil && err != http.ErrServerClosed {
 			return err
