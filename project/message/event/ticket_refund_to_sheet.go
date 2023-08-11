d6cffd8344b06775e6d01dde6a258d139642ff4b
coobeet <coobeet@gmail.com>
2023-08-12 00:00:39 +0800

    feat: 09-07

--- project/message/event/ticket_refund_to_sheet.go
+++ project/message/event/ticket_refund_to_sheet.go
@@ -2,13 +2,12 @@ package event
 
 import (
 	"context"
-
 	"tickets/entities"
 
 	"github.com/ThreeDotsLabs/go-event-driven/common/log"
 )
 
-func (h Handler) CancelTicket(ctx context.Context, event entities.TicketBookingCanceled) error {
+func (h Handler) TicketRefundToSheet(ctx context.Context, event *entities.TicketBookingCanceled) error {
 	log.FromContext(ctx).Info("Adding ticket refund to sheet")
 
 	return h.spreadsheetsService.AppendRow(
