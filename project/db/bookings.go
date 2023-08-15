e66663d37c2008edc23d3dabef9a48c8ffb02088
coobeet <coobeet@gmail.com>
2023-08-15 23:17:13 +0800

    feat: 11-10

--- project/db/bookings.go
+++ project/db/bookings.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
 	"tickets/entities"
 	"tickets/message/event"
 	"tickets/message/outbox"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 type BookingsRepository struct {
@@ -37,6 +39,37 @@ func (b BookingsRepository) AddBooking(ctx context.Context, booking entities.Boo
 		}
 		err = tx.Commit()
 	}()
+
+	availableSeats := 0
+	err = tx.GetContext(ctx, &availableSeats, `
+		SELECT
+		    number_of_tickets AS available_seats
+		FROM
+		    shows
+		WHERE
+		    show_id = $1
+	`, booking.ShowID)
+	if err != nil {
+		return fmt.Errorf("could not get available seats: %w", err)
+	}
+
+	alreadyBookedSeats := 0
+	err = tx.GetContext(ctx, &alreadyBookedSeats, `
+		SELECT
+		    coalesce(SUM(number_of_tickets), 0) AS already_booked_seats
+		FROM
+		    bookings
+		WHERE
+		    show_id = $1
+	`, booking.ShowID)
+	if err != nil {
+		return fmt.Errorf("could not get already booked seats: %w", err)
+	}
+
+	if availableSeats-alreadyBookedSeats < booking.NumberOfTickets {
+		// this is usually a bad idea, learn more here: https://threedots.tech/post/introducing-clean-architecture/
+		return echo.NewHTTPError(http.StatusBadRequest, "not enough seats available")
+	}
 
 	_, err = tx.NamedExecContext(ctx, `
 		INSERT INTO 
